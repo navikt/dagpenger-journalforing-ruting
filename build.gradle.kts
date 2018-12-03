95b1c80eb36a83ef3759ff689e3a63fa65cd68ce
Nico Kuijlen <nico.kuijlen@nav.no>
2018-12-03 09:21:28 +0100

    Temporarily add mavenLocal() until dagpenger-metrics is published

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ apply {
 
 repositories {
     mavenCentral()
+    mavenLocal()
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
     maven("http://packages.confluent.io/maven/")
     maven("https://dl.bintray.com/kotlin/ktor/")
