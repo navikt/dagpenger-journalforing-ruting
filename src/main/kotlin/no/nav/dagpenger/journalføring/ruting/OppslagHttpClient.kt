81d13f9fbd4ade4b7b86ad3cc31a7d419ca80f09
Atle Nordland <atle.nordland@nav.no>
2018-12-14 14:35:28 +0100

    Add http retry

--- src/main/kotlin/no/nav/dagpenger/journalføring/ruting/OppslagHttpClient.kt
+++ src/main/kotlin/no/nav/dagpenger/journalføring/ruting/OppslagHttpClient.kt
@@ -5,6 +5,7 @@ import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.result.Result
 import com.google.gson.Gson
 import mu.KotlinLogging
+import no.nav.dagpenger.http.retryFuelHttp
 
 private val LOGGER = KotlinLogging.logger {}
 
@@ -13,25 +14,29 @@ class OppslagHttpClient(private val oppslagUrl: String) : OppslagClient {
     override fun hentGeografiskTilknytning(request: GeografiskTilknytningRequest): GeografiskTilknytningResponse {
         val url = "$oppslagUrl/person/geografisk-tilknytning"
         val json = Gson().toJson(request).toString()
-        val (_, response, result) = with(url.httpPost()
-                .header(mapOf("Content-Type" to "application/json"))
-                .body(json)) {
+        val (_, response, result) = retryFuelHttp {
+                with(url.httpPost()
+                        .header(mapOf("Content-Type" to "application/json"))
+                        .body(json)) {
                 responseObject<GeografiskTilknytningResponse>()
+            }
         }
         return when (result) {
-            is Result.Failure -> throw OppslagException(
-                    response.statusCode, response.responseMessage, result.getException())
-            is Result.Success -> result.get()
+                is Result.Failure -> throw OppslagException(
+                        response.statusCode, response.responseMessage, result.getException())
+                is Result.Success -> result.get()
         }
     }
 
     override fun hentBehandlendeEnhet(request: BehandlendeEnhetRequest): BehandlendeEnhetResponse {
         val url = "$oppslagUrl/arbeidsfordeling/behandlende-enhet"
         val json = Gson().toJson(request).toString()
-        val (_, response, result) = with(url.httpPost()
-                    .header(mapOf("Content-Type" to "application/json"))
-                    .body(json)) {
+        val (_, response, result) = retryFuelHttp {
+                with(url.httpPost()
+                        .header(mapOf("Content-Type" to "application/json"))
+                        .body(json)) {
                 responseObject<BehandlendeEnhetResponse>()
+            }
         }
         return when (result) {
             is Result.Failure -> throw OppslagException(
