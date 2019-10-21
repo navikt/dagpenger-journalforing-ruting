813a3733f3605f1326858531b2eaf1f9b369327b
Giao The Cung <giaothe@gmail.com>
2019-10-21 23:00:44 +0200

    Fix typo in script.

--- init-scripts/03-export-service-user.sh
+++ init-scripts/03-export-service-user.sh
@@ -4,7 +4,7 @@ if test -f /secrets/serviceuser/srvdagpenger-journ/username;
 then
     export  SRVDAGPENGER_JOURNALFORING_RUTING_USERNAME=$(cat /secrets/serviceuser/srvdagpenger-journ/username)
 fi
-if test -f /secrets/serviceuserver/srvdagpenger-journ/password;
+if test -f /secrets/serviceuser/srvdagpenger-journ/password;
 then
     export  SRVDAGPENGER_JOURNALFORING_RUTING_PASSWORD=$(cat /secrets/serviceuser/srvdagpenger-journ/password)
 fi
